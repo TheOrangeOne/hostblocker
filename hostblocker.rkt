33fa85c6f92e8864038f96762f6b6179b17ecdfa
theorangeone <kyleverhoog@gmail.com>
2016-05-21 20:20:03 -0400

    use parameters

--- hostblocker.rkt
+++ hostblocker.rkt
@@ -5,6 +5,8 @@
 (require net/head)
 (require net/url)
 
+(error-print-context-length 0)
+
 
 (define-struct loe (entries))
 
@@ -18,27 +20,37 @@
 
 ;; (fetch-hostfile url) -> (listof string)
 ;; GET hostfile located at url
-(define (get-hostfile url)
+(define (get-remote-hostfile url)
   (with-handlers
     ([(λ (x) #t)
-      (λ (x) (error (string-append "ERRORi could not connect to " url)))])
+      (λ (x) (error (string-append "ERROR: could not connect to " url)))])
     (define-values (status header resp)
       (http-sendrecv/url (string->url url) #:method "GET"))
     (pipe->los resp)))
 
+(define (read-hostsfile file)
+  (define los (pipe->los file))
+  (void (map displayln los)))
 
-(define hosts-file (make-parameter "/etc/hosts"))
+
+(define (main)
+  (define hostsfile (open-input-file (hostsfile-path)))
+  (read-hostsfile hostsfile))
+
+
+(define hostsfile-path (make-parameter "/etc/hosts"))
+(define remote-src (make-parameter ""))
 
 ;; define commandline
-(define file
+(define cmd
   (command-line
    #:program "hostblocker"
    #:once-each
    [("-f" "--file") filename
                     "specify hosts file: <filename>"
-                    (hosts-file filename)]
-   #:once-each
+                    (hostsfile-path filename)]
    [("-g" "--get") location
-                   "gets the host file at <location>"
-    (void (map displayln (get-hostfile location)))]
-   #:args (filename) (hosts-file filename)))
+                   "gets the host file at <location> and adds it as a source"
+                   (remote-src location)]))
+
+(main)
