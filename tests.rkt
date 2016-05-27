664df1d79945e9062aff95f3f9df3982bbf1f5ac
theorangeone <kyleverhoog@gmail.com>
2016-05-27 01:20:59 -0400

    begin writing tests for hostsfile-parse

--- tests.rkt
+++ tests.rkt
@@ -7,20 +7,44 @@
 (require "hostblocker.rkt")
 
 
+(define (los->input-port los)
+  (open-input-string (string-join los "\n")))
+
 (define (in-list? val lst)
   (if (list-index (curry equal? val) lst) #t #f))
+
+
+
+(define simple-hf-los
+  '( "#! src: http://adaway.org/hosts.txt"
+     "0.0.0.0 ad-g.doubleclick.net       #! ads crap"
+     "0.0.0.0 adsense.com                #! ads"
+     "#! end src"))
+(define simple-hf (los->input-port simple-hf-los))
+
+(define multiple-srcs-hf-los
+  '( "#! src: http://adaway.org/hosts.txt"
+     "0.0.0.0 ad-g.doubleclick.net       #! ads crap"
+     "0.0.0.0 adsense.com                #! ads"
+     "#! end src"
+     ""
+     " "
+     "             "
+     "#! src: /home/user/myhostsfile"
+     "0.0.0.0 facebook.com               #! garbage crap"
+     "0.0.0.0 twitter.com                #! useless trash"
+     "0.0.0.0 reddit.com"
+     "#! end src"))
+(define multiple-srcs-hf (los->input-port multiple-srcs-hf-los))
+
+
 
 
 ;; TODO find a better way to test hash-maps
 (test-case
     "generate-sources simple hostsfile"
 
-  (define hosts-file-los
-    '( "#! src: http://adaway.org/hosts.txt"
-       "0.0.0.0 ad-g.doubleclick.net       #! ads crap"
-       "0.0.0.0 adsense.com                #! ads"
-       "#! end src"))
-  (define sources (generate-sources hosts-file-los))
+  (define sources (generate-sources simple-hf-los))
   (check-true (hash? sources) "Test passed")
   (check-true (hash-has-key? sources "http://adaway.org/hosts.txt"))
 
@@ -35,20 +59,7 @@
 (test-case
     "generate-sources multiple sources hostsfile"
 
-  (define hosts-file-los
-    '( "#! src: http://adaway.org/hosts.txt"
-       "0.0.0.0 ad-g.doubleclick.net       #! ads crap"
-       "0.0.0.0 adsense.com                #! ads"
-       "#! end src"
-       ""
-       " "
-       "             "
-       "#! src: /home/user/myhostsfile"
-       "0.0.0.0 facebook.com               #! garbage crap"
-       "0.0.0.0 twitter.com                #! useless trash"
-       "0.0.0.0 reddit.com"
-       "#! end src"))
-  (define sources (generate-sources hosts-file-los))
+  (define sources (generate-sources multiple-srcs-hf-los))
   (check-true (hash? sources))
   (check-true (hash-has-key? sources "http://adaway.org/hosts.txt"))
 
@@ -71,3 +82,10 @@
   (check-equal? (hash-ref mhf-src "facebook.com") '("garbage" "crap"))
   (check-equal? (hash-ref mhf-src "twitter.com") '("useless" "trash"))
   (check-equal? (hash-ref mhf-src "reddit.com") '()))
+
+;; TODO finish this test
+(test-case
+    "hostsfile-parse simple hostsfile"
+  (define hf (hostsfile-parse simple-hf))
+  (define-values (entries sources tags orig) (hostsfile-values hf))
+  (check-equal? simple-hf-los (reverse orig)))
